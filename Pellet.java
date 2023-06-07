e938d08fc8fd2ec7b885f32102a9df1d3dc82bfd
dhoodi-1 <131403168+dhoodi-1@users.noreply.github.com>
2023-06-07 19:21:00 -0400

    cooked

--- Pellet.java
+++ Pellet.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.util.ArrayList;
 
 public class Pellet extends Circle{
     
@@ -6,15 +7,27 @@ public class Pellet extends Circle{
         super(5,  xx,  yy, Color.YELLOW);
     }
 
-    public boolean underPac(Pacman p){
-        if(p.getX() == getX() && p.getY() == getY()){
+    public boolean underPac(Pacman p, ArrayList<Pellet> pp){
+        for(int i = 0; i<pp.size(); i++){
+        if((p.getX() < pp.get(i).getX() && (p.getX()+p.getSide()) > pp.get(i).getX()) && 
+        (p.getY() < pp.get(i).getY() && (p.getY()+p.getSide()) > pp.get(i).getY())){
             return true;
         }
         
         return false;
     }
+        return false;
+    }
 
-
+    // boolean overlapsGhost(double x, double y, int width, int height, ArrayList<Ghost> ghosts) {
+    //     for (int i = 0; i < ghosts.size(); i++) {
+    //     if (x < ghosts.get(i).getX() + ghosts.get(i).getWidth() && x + width > ghosts.get(i).getX()
+    //     && y < ghosts.get(i).getY() + ghosts.get(i).getHeight() && y + height > ghosts.get(i).getY()) {
+    //     return true;
+    //     }
+    //     }
+    //     return false;
+    //     }
 
 
 
